67a094970ba74e6d4ff3122fb228990057d6f8f2
최종명 <jmchoi@douzone.com>
2022-07-12 11:41:05 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# eslint-config-zmzhoi
+
+> This is a `Shareable Config`.
+
+This package's dependencies:
+
+```
+{
+  "eslint-config-airbnb": "^19",
+  "eslint-config-prettier": "^8.5.0"
+}
+```
+
+> Eslint version : `^7.32.0` || `^8.2.0`
+
+# Usage
+
+Installation:
+
+```
+  npm install -D eslint-config-zmzhoi
+```
+
+Then create `.eslintrc.js` file with following contents in the root folder of your project:
+
+.eslintrc.js
+
+```
+module.exports = {
+  extends: ['zmzhoi'],
+};
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  rules: {
+    'no-unused-vars': 1,
+    'no-console': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/forbid-prop-types': 0,
+    'no-restricted-exports': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'no-shadow': 0,
+    'react/no-array-index-key': 0,
+    'no-debugger': 0,
+    'no-underscore-dangle': 0,
+    'import/prefer-default-export': 0,
+    'no-return-assign': 0,
+    'arrow-body-style': 0,
+    'no-param-reassign': 0,
+    'consistent-return': 0,
+    'import/no-named-as-default-member': 0,
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent', 'index'],
+          'object',
+        ],
+        pathGroups: [
+          {
+            pattern: '@**/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+  },
+};
