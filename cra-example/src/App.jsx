7d4c57a10f8b9c356ffe2da80ca56cdcd52f7c63
최종명 <jmchoi@douzone.com>
2022-07-13 14:10:19 +0900

    change: cra-example 코드 수정

--- cra-example/src/App.jsx
+++ cra-example/src/App.jsx
@@ -1,12 +1,30 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [flag, setFlag] = useState(false);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log('hi');
-  }, []);
-  return <div className="App">This is a app</div>;
+    console.log(count, setCount);
+  }, [count]);
+
+  return (
+    <div className="App">
+      <div>
+        <button type="button" onClick={() => setCount((c) => c + 1)}>
+          +
+        </button>
+        <button type="button" onClick={() => setCount((c) => c - 1)}>
+          -
+        </button>
+        <button type="button" onClick={() => setCount(0)}>
+          Clear
+        </button>
+      </div>
+      <div>
+        <span>{count}</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
